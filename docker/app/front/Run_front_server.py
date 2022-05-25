a159022b70a954361dbd0207d90bbd2bdd998fc4
Ildar Khusainov <khusainov.ildar.rish@gmail.com>
2022-05-25 12:24:43 +0500

    add descriptions and comments

--- docker/app/front/Run_front_server.py
+++ docker/app/front/Run_front_server.py
@@ -12,10 +12,12 @@ import urllib.request
 import json
 
 
+# create form fo text input and validate not empty field
 class ClientDataForm(FlaskForm):
     comment_text = StringField("Type some text:", validators=[DataRequired()])
 
 
+# initialize Flask
 app = Flask(__name__)
 app.config.update(
     CSRF_ENABLED=True,
@@ -23,43 +25,55 @@ app.config.update(
 )
 
 
+# get predictions from main server (there is a model.dill)
 def get_prediction(comment_text):
     body = {'comment_text': comment_text}
-
+    # make url to main server
     myurl = "http://0.0.0.0:8181/predict"
     req = urllib.request.Request(myurl)
     req.add_header('Content-Type', 'application/json; charset=utf-8')
+    # encode json
     jsondata = json.dumps(body)
-    jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
-    req.add_header('Content-Length', len(jsondataasbytes))
+    # needs to be bytes
+    jsondataasbytes = jsondata.encode('utf-8')
+    req.add_header('Content-Length', str(len(jsondataasbytes)))
+    # get response from main server
     response = urllib.request.urlopen(req, jsondataasbytes)
+    # decode json and return predictions list
     return json.loads(response.read())['predictions']
 
 
+# create index.html
 @app.route("/")
 def index():
     return render_template('index.html')
 
 
+# create predicted.html
 @app.route('/predicted/<response>')
 def predicted(response):
+    # decode json
     response = json.loads(response)
     print(response)
     return render_template('predicted.html', response=response)
 
 
+# create predict_form.html
 @app.route('/predict_form', methods=['GET', 'POST'])
 def predict_form():
     form = ClientDataForm()
     data = dict()
     if request.method == 'POST':
+        # get comment text from request
         data['comment_text'] = request.form.get('comment_text')
 
+        # try if not errors
         try:
+            # get predictions list from main server
             preds = get_prediction(data['comment_text'])
 
             # Draw probabilities distribution.
-            # It is working in my machine, but not work in docker.
+            # It is working in my machine, but not work into docker.
             # color = ['red', 'blue', 'green', 'yellow', 'black', 'orange']
             # columns = np.array(['tx', 's_tx', 'obs', 'thr', 'ins', 'i_ht'])
             # plt.ioff()
@@ -70,14 +84,18 @@ def predict_form():
             # plt.savefig('static/probabilities.png')
             # plt.close()
 
+            # round predictions
             response = str([round(i, 2) for i in preds])
         except ConnectionError:
             response = json.dumps({"error": "ConnectionError"})
 
+        # go to predicted.html
         return redirect(url_for('predicted', response=response))
 
+    # render predict_form.html
     return render_template('form.html', form=form)
 
 
+# initialize front server
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8180, debug=True)
